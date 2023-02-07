6576c5b176305a91fa2457a276207225f88adcc8
Vladimir Zorov <vl.zorov@gmail.com>
2023-02-07 19:03:53 +0200

    add files

---
+++ src/easterRaces/core/ControllerImpl.java
@@ -0,0 +1,38 @@
+package easterRaces.core;
+
+import easterRaces.core.interfaces.Controller;
+
+public class ControllerImpl implements Controller {
+
+
+
+    @Override
+    public String createDriver(String driver) {
+        return null;
+    }
+
+    @Override
+    public String createCar(String type, String model, int horsePower) {
+        return null;
+    }
+
+    @Override
+    public String addCarToDriver(String driverName, String carModel) {
+        return null;
+    }
+
+    @Override
+    public String addDriverToRace(String raceName, String driverName) {
+        return null;
+    }
+
+    @Override
+    public String startRace(String raceName) {
+        return null;
+    }
+
+    @Override
+    public String createRace(String name, int laps) {
+        return null;
+    }
+}
